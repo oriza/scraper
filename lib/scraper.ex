2663aee45bb28147ec87f6ab0ec04813c45fe754
László Horváth <bedafoltyn@gmail.com>
2019-04-11 10:42:20 +0200

    api refactored to use html and json parsers

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -5,8 +5,8 @@ defmodule Scraper do
 
   defdelegate scrape_archive(http_client, url, selectors), to: Scraper.Archive, as: :scrape
   defdelegate scrape_article(http_client, html_parser, url, selectors), to: Scraper.Article, as: :scrape
-  defdelegate scrape_gemius(http_client, url, sites), to: Scraper.Gemius, as: :scrape
-  defdelegate scrape_rates(http_client, url), to: Scraper.Rates, as: :scrape
-  defdelegate scrape_rss(http_client, html_scraper, url), to: Scraper.Rss, as: :scrape
-  defdelegate scrape_weather(http_client, city), to: Scraper.Weather, as: :scrape
+  defdelegate scrape_gemius(http_client, json_parser, url, sites), to: Scraper.Gemius, as: :scrape
+  defdelegate scrape_rates(http_client, html_parser, url), to: Scraper.Rates, as: :scrape
+  defdelegate scrape_rss(http_client, html_parser, url), to: Scraper.Rss, as: :scrape
+  defdelegate scrape_weather(http_client, json_parsre, city), to: Scraper.Weather, as: :scrape
 end
