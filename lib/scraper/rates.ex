c60abfe7e63a9c75a4aac2d57d0c796724010393
László Horváth <bedafoltyn@gmail.com>
2019-04-11 10:40:46 +0200

    rates refactored to use html parser

--- lib/scraper/rates.ex
+++ lib/scraper/rates.ex
@@ -6,31 +6,29 @@ defmodule Scraper.Rates do
     iex> Scraper.Rates.scrape("api.napiarfolyam.hu/?bank=mnb")
   """
 
-  import Meeseeks.CSS
-
-  def scrape(http_client, bank) do
+  def scrape(http_client, html_parser, bank) do
     "http://api.napiarfolyam.hu/?bank=#{bank}"
     |> http_client.get()
-    |> extract()
+    |> extract(html_parser)
   end
 
-  defp extract({:ok, body}) do
-    document = Meeseeks.parse(body)
+  defp extract({:ok, body}, html_parser) do
+    document = html_parser.parse(body)
 
-    for rate <- Meeseeks.all(document, css("arfolyamok deviza item")) do
-      currency = Meeseeks.one(rate, css("penznem"))
-      buy = Meeseeks.one(rate, css("vetel"))
-      sell = Meeseeks.one(rate, css("eladas"))
-      average = Meeseeks.one(rate, css("kozep"))
+    for rate <- html_parser.query_selector_all(document, "arfolyamok deviza item") do
+      currency = html_parser.query_selector(rate, "penznem")
+      buy = html_parser.query_selector(rate, "vetel")
+      sell = html_parser.query_selector(rate, "eladas")
+      average = html_parser.query_selector(rate, "kozep")
 
       %{
-        currency: Meeseeks.text(currency),
-        buy: Meeseeks.text(buy),
-        sell: Meeseeks.text(sell),
-        average: Meeseeks.text(average)
+        currency: html_parser.text(currency),
+        buy: html_parser.text(buy),
+        sell: html_parser.text(sell),
+        average: html_parser.text(average)
       }
     end
   end
 
-  defp extract({:error, reason}), do: {:error, reason}
+  defp extract({:error, reason}, _), do: {:error, reason}
 end
